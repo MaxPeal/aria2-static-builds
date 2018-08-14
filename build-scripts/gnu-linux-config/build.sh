5c7920b524cc869538dd27ba5b8d7e290f4ab60b
tofuliang <tofuliang@gmail.com>
2018-08-14 15:08:33 +0800

    add max-http-forbidden

--- build-scripts/gnu-linux-config/build.sh
+++ build-scripts/gnu-linux-config/build.sh
@@ -4,7 +4,7 @@ apk update && apk add g++ gcc make wget ca-certificates file perl
 
 sh /usr/local/src/aria2-static-builds/build-scripts/gnu-linux-config/aria2-x86_64-gnu-linux-build-libs
 cd /tmp;
-wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.xz;
+[ ! -f aria2-1.34.0.tar.xz ] && wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.xz;
 tar xf aria2-1.34.0.tar.xz;
 cd aria2-1.34.0;
 sh /usr/local/src/aria2-static-builds/build-scripts/gnu-linux-config/aria2-x86_64-gnu-linux-config
