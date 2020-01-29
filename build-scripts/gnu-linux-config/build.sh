cb90b7192adafdefef2a1991b0c89557c2dbf7e3
tofuliang <tofuliang@gmail.com>
2020-01-29 18:06:38 +0800

    use alpine 3.11

--- build-scripts/gnu-linux-config/build.sh
+++ build-scripts/gnu-linux-config/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-apk update && apk add g++ gcc make wget ca-certificates file perl
+apk update && apk add g++ gcc make wget ca-certificates file perl linux-headers pkgconf
 
 sh /usr/local/src/aria2-static-builds/build-scripts/gnu-linux-config/aria2-x86_64-gnu-linux-build-libs
 cd /tmp;
-[ ! -f aria2-1.34.0.tar.xz ] && wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.xz;
-tar xf aria2-1.34.0.tar.xz;
-cd aria2-1.34.0;
+[ ! -f aria2-1.35.0.tar.xz ] && wget https://github.com/aria2/aria2/releases/download/release-1.35.0/aria2-1.35.0.tar.xz;
+tar xf aria2-1.35.0.tar.xz;
+cd aria2-1.35.0;
+
 sh /usr/local/src/aria2-static-builds/build-scripts/gnu-linux-config/aria2-x86_64-gnu-linux-config
 make -j`grep -c ^processor /proc/cpuinfo`
 strip src/aria2c
