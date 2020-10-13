1ae94cdc07144a488b766ab51bad29eaadab19ba
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2020-10-13 18:33:03 +0200

    Update build.sh

--- build-scripts/gnu-linux-config/build.sh
+++ build-scripts/gnu-linux-config/build.sh
@@ -4,9 +4,9 @@ apk update && apk add g++ gcc make wget ca-certificates file perl linux-headers 
 apk add --no-cache --virtual .build-deps ca-certificates gnutls-dev expat-dev sqlite-dev c-ares-dev cppunit-dev bash-completion openrc xz-libs autoconf automake curl-dev
 apk stats
 # debian a like
-cd /usr/local/sbin &&  ln -s /usr/lib/ccache/* .
+cd /usr/local/sbin &&  (cd /usr/local/sbin && ln -s /usr/lib/ccache/* . || true )
 # alpine linux testet on 3.11
-cd /usr/local/bin && ln -s /usr/lib/ccache/bin/* .
+cd /usr/local/bin && (cd /usr/local/sbin && ln -s /usr/lib/ccache/bin/* . || true )
 #/tmp/ccache
 #ccache -M 2G
 ccache -p || ccache --print-config
